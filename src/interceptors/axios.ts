267f13f9cbb27cdf026177f3db3a2c1668bc8c6d
6hiro <hiro6github@gmail.com>
2022-04-11 08:19:55 +0900

    first commit

--- src/interceptors/axios.ts
+++ src/interceptors/axios.ts
@@ -50,24 +50,3 @@ axios.interceptors.response.use(resp => resp, async error => {
     return error;
 });
 
-
-// let refresh = false;
-
-// axios.interceptors.response.use(resp => resp, async error => {
-//     if (error.response.status === 403 && !refresh) {
-//         refresh = true;
-
-//         const response = await axios.post('refresh/', {}, {withCredentials: true});
-
-//         if (response.status === 200) {
-//             localStorage.removeItem("localJWT");
-//             localStorage.setItem("localJWT", response.data.token);
-//             // axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
-//             error.config.headers.Authorization = `Bearer ${response.data.token}`
-
-//             return axios(error.config);
-//         }
-//     }
-//     refresh = false;
-//     return error;
-// });
